168fde03199e1a7066febd0761c473baf89a0afa
yanggy <ilike.yanggy@gmail.com>
2023-11-05 15:13:01 +0800

    动态代理

--- frpc-spi/src/main/java/com/freeing/rpc/spi/factory/SpiExtensionFactory.java
+++ frpc-spi/src/main/java/com/freeing/rpc/spi/factory/SpiExtensionFactory.java
@@ -1,5 +1,10 @@
 package com.freeing.rpc.spi.factory;
 
+import com.freeing.rpc.spi.annotation.SPI;
+import com.freeing.rpc.spi.loader.ExtensionLoader;
+
+import java.util.Optional;
+
 /**
  * 扩展类加载工厂接口
  */
@@ -7,6 +12,11 @@ public class SpiExtensionFactory implements ExtensionFactory {
 
     @Override
     public <T> T getExtension(String key, Class<T> clazz) {
-        return null;
+        return Optional.ofNullable(clazz)
+            .filter(Class::isInterface)
+            .filter(cls -> cls.isAnnotationPresent(SPI.class))
+            .map(ExtensionLoader::getExtensionLoader)
+            .map(ExtensionLoader::getDefaultSpiClassInstance)
+            .orElse(null);
     }
 }
