9a18e4fdece630d7432bb299300334c2edb8d572
yanggy <ilike.yanggy@gmail.com>
2023-11-06 20:36:32 +0800

    负载均衡

---
+++ frpc-loadbalancer/frpc-loadbalancer-random-weight/src/main/java/com/freeing/rpc/loadbalancer/random/weight/RandomWeightServiceLoadBalancer.java
@@ -0,0 +1,36 @@
+package com.freeing.rpc.loadbalancer.random.weight;
+
+import com.freeing.loadbalancer.api.ServiceLoadBalancer;
+import com.freeing.rpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
+/**
+ * 加权随机
+ *
+ * @author yanggy
+ */
+@SPIClass
+public class RandomWeightServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+    private static final Logger logger = LoggerFactory.getLogger(RandomWeightServiceLoadBalancer.class);
+
+    @Override
+    public T select(List<T> servers, int hashcode) {
+        logger.info("select server base RandomWeightServiceLoadBalancer.class");
+        if (Objects.isNull(servers) || servers.isEmpty()) {
+            return null;
+        }
+        hashcode = Math.abs(hashcode);
+        int count = hashcode % servers.size();
+        if (count <= 1){
+            count = servers.size();
+        }
+        Random random = new Random();
+        int index = random.nextInt(count);
+        return servers.get(index);
+    }
+}
