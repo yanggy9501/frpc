b6e86feaf17c225110addf4d3fb6db2a42b64b66
yanggy <ilike.yanggy@gmail.com>
2023-11-07 21:53:52 +0800

    负载均衡

---
+++ frpc-loadbalancer/frpc-loadbalancer-round-robin-weight/src/main/java/com/freeing/rpc/loadbalancer/robin/weight/RobinWeightServiceLoadBalancer.java
@@ -0,0 +1,38 @@
+package com.freeing.rpc.loadbalancer.robin.weight;
+
+import com.freeing.loadbalancer.api.ServiceLoadBalancer;
+import com.freeing.rpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author yanggy
+ */
+@SPIClass
+public class RobinWeightServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+    private static final Logger logger = LoggerFactory.getLogger(RobinWeightServiceLoadBalancer.class);
+
+    private final AtomicInteger next_index = new AtomicInteger(0);
+
+    @Override
+    public T select(List<T> servers, int hashcode) {
+        logger.info("select server base RobinWeightServiceLoadBalancer.class");
+        if (Objects.isNull(servers) || servers.isEmpty()) {
+            return null;
+        }
+        hashcode = Math.abs(hashcode);
+        int count = hashcode % servers.size();
+        if (count <= 0){
+            count = servers.size();
+        }
+        int index = next_index.incrementAndGet();
+        if (index >= Integer.MAX_VALUE - 10000){
+            next_index.set(0);
+        }
+        return servers.get(index % count);
+    }
+}
---
+++ frpc-loadbalancer/frpc-loadbalancer-round-robin/src/main/java/com/freeing/rpc/loadbalancer/robin/RobinServiceLoadBalancer.java
@@ -0,0 +1,37 @@
+package com.freeing.rpc.loadbalancer.robin;
+
+import com.freeing.loadbalancer.api.ServiceLoadBalancer;
+import com.freeing.rpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 基于轮询算法的负载均衡策略
+ *
+ * @author yanggy
+ */
+@SPIClass
+public class RobinServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+    private static final Logger logger = LoggerFactory.getLogger(RobinServiceLoadBalancer.class);
+
+    private final AtomicInteger nextIndex = new AtomicInteger();
+
+    @Override
+    public T select(List<T> servers, int hashcode) {
+        logger.info("select server base RobinServiceLoadBalancer.class");
+        if (Objects.isNull(servers) || servers.isEmpty()) {
+            return null;
+        }
+        int len = servers.size();
+        int index = nextIndex.getAndIncrement();
+        if (index < 0) {
+            nextIndex.set(0);
+            index = nextIndex.getAndIncrement();
+        }
+        return servers.get(index % len);
+    }
+}
