729fd9d5ee7d61041b5abd2fbeaec3b00a48afec
yanggy <ilike.yanggy@gmail.com>
2023-11-23 22:14:56 +0800

    整合 springboot

---
+++ frpc-spring-boot-starter/frpc-spring-boot-starter-consumer/src/main/java/com/freeing/rpc/spring/boot/consumer/SpringBootConsumerAutoConfiguration.java
@@ -0,0 +1,40 @@
+package com.freeing.rpc.spring.boot.consumer;
+
+import com.freeing.rpc.consumer.RpcClient;
+import com.freeing.rpc.spring.boot.consumer.config.SpringBootConsumerConfig;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yanggy
+ */
+@Configuration
+@EnableConfigurationProperties
+public class SpringBootConsumerAutoConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "frpc.consumer")
+    public SpringBootConsumerConfig springBootConsumerConfig() {
+        return new SpringBootConsumerConfig();
+    }
+
+    @Bean
+    public RpcClient rpcClient(final SpringBootConsumerConfig springBootConsumerConfig) {
+        return new RpcClient(springBootConsumerConfig.getRegistryAddress(),
+            springBootConsumerConfig.getRegistryType(),
+            springBootConsumerConfig.getLoadBalanceType(),
+            springBootConsumerConfig.getProxy(),
+            springBootConsumerConfig.getVersion(),
+            springBootConsumerConfig.getGroup(),
+            springBootConsumerConfig.getSerializationType(),
+            springBootConsumerConfig.getTimeout(),
+            springBootConsumerConfig.getAsync(),
+            springBootConsumerConfig.getOneway(),
+            springBootConsumerConfig.getHeartbeatInterval(),
+            springBootConsumerConfig.getScanNotActiveChannelInterval(),
+            springBootConsumerConfig.getRetryInterval(),
+            springBootConsumerConfig.getRetryTimes());
+    }
+}
---
+++ frpc-spring-boot-starter/frpc-spring-boot-starter-consumer/src/main/java/com/freeing/rpc/spring/boot/consumer/config/SpringBootConsumerConfig.java
@@ -0,0 +1,212 @@
+package com.freeing.rpc.spring.boot.consumer.config;
+
+/**
+ * SpringBootConsumerConfig
+ *
+ * @author yanggy
+ */
+public class SpringBootConsumerConfig {
+    /**
+     * 注册地址
+     */
+    private String registryAddress;
+
+    /**
+     * 注册类型
+     */
+    private String registryType;
+
+    /**
+     * 负载均衡类型
+     */
+    private String loadBalanceType;
+
+    /**
+     * 代理
+     */
+    private String proxy;
+
+    /**
+     * 版本号
+     */
+    private String version;
+
+    /**
+     * 分组
+     */
+    private String group;
+
+    /**
+     * 序列化类型
+     */
+    private String serializationType;
+
+    /**
+     * 超时时间
+     */
+    private int timeout;
+
+    /**
+     * 是否异步
+     */
+    private boolean async;
+
+    /**
+     * 是否单向调用
+     */
+    private boolean oneway;
+
+    /**
+     * 心跳检测
+     */
+    private int heartbeatInterval;
+
+    /**
+     * 扫描并移除不活跃连接的时间间隔
+     */
+    private int scanNotActiveChannelInterval;
+
+    // 重试间隔时间
+    private int retryInterval = 1000;
+
+    // 重试次数
+    private int retryTimes = 3;
+
+    public SpringBootConsumerConfig() {
+
+    }
+
+    public SpringBootConsumerConfig(final String registryAddress, final String registryType, final String loadBalanceType,
+        final String proxy, final String version, final String group, final String serializationType,
+        final int timeout, final boolean async, final boolean oneway, final int heartbeatInterval,
+        final int scanNotActiveChannelInterval, final int retryInterval, final int retryTimes) {
+        this.registryAddress = registryAddress;
+        this.registryType = registryType;
+        this.loadBalanceType = loadBalanceType;
+        this.proxy = proxy;
+        this.version = version;
+        this.group = group;
+        this.serializationType = serializationType;
+        this.timeout = timeout;
+        this.async = async;
+        this.oneway = oneway;
+        if (heartbeatInterval > 0){
+            this.heartbeatInterval = heartbeatInterval;
+        }
+        this.scanNotActiveChannelInterval = scanNotActiveChannelInterval;
+        this.retryInterval = retryInterval;
+        this.retryTimes = retryTimes;
+    }
+
+    public String getRegistryAddress() {
+        return registryAddress;
+    }
+
+    public void setRegistryAddress(String registryAddress) {
+        this.registryAddress = registryAddress;
+    }
+
+    public String getRegistryType() {
+        return registryType;
+    }
+
+    public void setRegistryType(String registryType) {
+        this.registryType = registryType;
+    }
+
+    public String getLoadBalanceType() {
+        return loadBalanceType;
+    }
+
+    public void setLoadBalanceType(String loadBalanceType) {
+        this.loadBalanceType = loadBalanceType;
+    }
+
+    public String getProxy() {
+        return proxy;
+    }
+
+    public void setProxy(String proxy) {
+        this.proxy = proxy;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getSerializationType() {
+        return serializationType;
+    }
+
+    public void setSerializationType(String serializationType) {
+        this.serializationType = serializationType;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    public boolean getAsync() {
+        return async;
+    }
+
+    public void setAsync(boolean async) {
+        this.async = async;
+    }
+
+    public boolean getOneway() {
+        return oneway;
+    }
+
+    public void setOneway(boolean oneway) {
+        this.oneway = oneway;
+    }
+
+    public int getHeartbeatInterval() {
+        return heartbeatInterval;
+    }
+
+    public void setHeartbeatInterval(int heartbeatInterval) {
+        this.heartbeatInterval = heartbeatInterval;
+    }
+
+    public int getRetryInterval() {
+        return retryInterval;
+    }
+
+    public void setRetryInterval(int retryInterval) {
+        this.retryInterval = retryInterval;
+    }
+
+    public int getRetryTimes() {
+        return retryTimes;
+    }
+
+    public void setRetryTimes(int retryTimes) {
+        this.retryTimes = retryTimes;
+    }
+
+    public int getScanNotActiveChannelInterval() {
+        return scanNotActiveChannelInterval;
+    }
+
+    public void setScanNotActiveChannelInterval(int scanNotActiveChannelInterval) {
+        this.scanNotActiveChannelInterval = scanNotActiveChannelInterval;
+    }
+}
